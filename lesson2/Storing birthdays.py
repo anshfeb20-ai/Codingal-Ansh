28f554557de3b4aa1d0921f8e0160c33eb01f055
ansh <anshfeb20@gmail.con>
2026-07-24 23:16:56 -0400

    lesson2

---
+++ lesson2/Storing birthdays.py
@@ -0,0 +1,12 @@
+Narayan="5th December"
+Aaliya="12th February"
+Gaurav="20th March"
+Ansh="20th February"
+Jasmine="13th April"
+
+
+print(Narayan)
+print(Aaliya)
+print(Gaurav)
+print(Ansh)
+print(Jasmine)
