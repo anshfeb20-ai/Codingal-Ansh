30f31c037ab82027753e4eeb91127d7afb3a84b3
ansh <anshfeb20@gmail.con>
2026-07-29 20:59:46 -0400

    lesson3

---
+++ lesson3/activity2.py
@@ -0,0 +1,13 @@
+grade=10
+school="Belmont High School"
+school_rating=4.5
+living_in_Belmont=True
+
+print(grade)
+print("type of grade:", type(grade))
+print(school)
+print("type of school:", type(school))
+print(school_rating)
+print("type of school_rating:", type(school_rating))
+print(living_in_Belmont)
+print("type of living_in_Belmont:", type(living_in_Belmont))
