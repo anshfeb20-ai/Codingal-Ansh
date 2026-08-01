61945380738710ba0da957cf3f6e3e7247c50af4
ansh <anshfeb20@gmail.con>
2026-07-31 22:09:09 -0400

    lesson4

---
+++ lesson4/activity3.py
@@ -0,0 +1,8 @@
+Maths=int(input("Enter your marks in Maths: "))
+Science=int(input("Enter your marks in Science: "))
+English=int(input("Enter your marks in English: "))
+Hindi=int(input("Enter your marks in Hindi: "))
+
+Sum=Maths+Science+English+Hindi
+Percentage=(Sum/400)*100
+print("Final Grade: ",Percentage)
