8477f3cc982c2afc684b51e676f976ab5820ac4d
ansh <anshfeb20@gmail.con>
2026-08-19 22:45:09 -0400

    All projects

---
+++ All projects/alphabetchecker.py
@@ -0,0 +1,5 @@
+character_entered_by_user = input("Please enter a character: ")
+if character_entered_by_user.isalpha():
+    print("You entered a alphabet.")
+else:
+    print("You did not enter an alphabet.")
