b17935338a2be77ad68709150696403f870c3190
ansh <anshfeb20@gmail.con>
2026-08-21 22:17:54 -0400

    lesson2

---
+++ Module2/lesson2/activity1.py
@@ -0,0 +1,5 @@
+integer1=int(input("Enter an integer: "))
+sum1=0
+for i in range(1, integer1 + 1):
+    sum1=sum1+i
+print(sum1)
---
+++ Module2/lesson2/activity2.py
@@ -0,0 +1,6 @@
+string=input("Enter a string: ")#HELLO
+revstring=""
+for i in string:
+    revstring=i
+print(revstring)
+
