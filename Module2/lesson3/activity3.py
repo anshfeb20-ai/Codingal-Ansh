0d6a5316773b776d0442eab90b4a062e58a164b7
ansh <anshfeb20@gmail.con>
2026-08-28 21:54:03 -0400

    lesson3

--- Module2/lesson3/activity3.py
+++ Module2/lesson3/activity3.py
@@ -1,2 +1,12 @@
 number=int(input("Enter a number: "))
 sum=0
+temp=number
+p=len(str(number))
+while number>0:
+    digit=number%10
+    sum=sum+(digit**p)
+    number=number//10
+if sum==temp:
+    print(temp," is an Armstrong Number")
+else:
+    print(temp," is not an Armstrong Number")
