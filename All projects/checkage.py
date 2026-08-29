106cc518b96cd0e9cc21798dcb5f2567826f8203
ansh <anshfeb20@gmail.con>
2026-08-28 20:24:09 -0400

    All projects

---
+++ All projects/checkage.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age: "))
+if age>=10:
+    if age<=20:
+        print("You are eligible to join Mr.Raj's 10th grade class.")
+    else:
+        print("You are not eligible to join Mr.Raj's 10th grade class.")
+else:
+    print("You are not eligible to join Mr.Raj's 10th grade class.")
